ad3a52bb316ef98ecaa93ced357b5d6a13aa811e
Sam Walton <sam@nicheprovider.com>
2017-07-07 07:39:59 -0500

    initial commit

---
+++ receiver_sam.ino
@@ -0,0 +1,196 @@
+#include <SPIFlash.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_IS31FL3731.h>
+Adafruit_IS31FL3731_Wing matrix = Adafruit_IS31FL3731_Wing();
+
+
+
+/* RFM69 library and code by Felix Rusu - felix@lowpowerlab.com
+// Get libraries at: https://github.com/LowPowerLab/
+// Make sure you adjust the settings in the configuration section below !!!
+// **********************************************************************************
+// Copyright Felix Rusu, LowPowerLab.com
+// Library and code by Felix Rusu - felix@lowpowerlab.com
+// **********************************************************************************
+// License
+// **********************************************************************************
+// This program is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU General    
+// Public License as published by the Free Software       
+// Foundation; either version 3 of the License, or        
+// (at your option) any later version.                    
+//                                                        
+// This program is distributed in the hope that it will   
+// be useful, but WITHOUT ANY WARRANTY; without even the  
+// implied warranty of MERCHANTABILITY or FITNESS FOR A   
+// PARTICULAR PURPOSE. See the GNU General Public        
+// License for more details.                              
+//                                                        
+// You should have received a copy of the GNU General    
+// Public License along with this program.
+// If not, see <http://www.gnu.org/licenses></http:>.
+//                                                        
+// Licence can be viewed at                               
+// http://www.gnu.org/licenses/gpl-3.0.txt
+//
+// Please maintain this license information along with authorship
+// and copyright notices in any redistribution of this code
+// **********************************************************************************/
+
+#include <RFM69.h>    //get it here: https://www.github.com/lowpowerlab/rfm69
+#include <SPI.h>
+
+//*********************************************************************************************
+// *********** IMPORTANT SETTINGS - YOU MUST CHANGE/ONFIGURE TO FIT YOUR HARDWARE *************
+//*********************************************************************************************
+#define NETWORKID     100  //the same on all nodes that talk to each other
+#define NODEID        1  
+
+//Match frequency to the hardware version of the radio on your Feather
+#define FREQUENCY     RF69_433MHZ
+//#define FREQUENCY     RF69_868MHZ
+//#define FREQUENCY      RF69_915MHZ
+#define ENCRYPTKEY     "sampleEncryptKey" //exactly the same 16 characters/bytes on all nodes!
+#define IS_RFM69HCW    true // set to 'true' if you are using an RFM69HCW module
+
+//*********************************************************************************************
+#define SERIAL_BAUD   9600 //115200
+
+/* for Feather 32u4 */
+#define RFM69_CS      8
+#define RFM69_IRQ     7
+#define RFM69_IRQN    4  // Pin 7 is IRQ 4!
+#define RFM69_RST     4
+
+/* for Feather M0 
+#define RFM69_CS      8
+#define RFM69_IRQ     3
+#define RFM69_IRQN    3  // Pin 3 is IRQ 3!
+#define RFM69_RST     4
+*/
+
+/* ESP8266 feather w/wing
+#define RFM69_CS      2
+#define RFM69_IRQ     15
+#define RFM69_IRQN    digitalPinToInterrupt(RFM69_IRQ )
+#define RFM69_RST     16
+*/
+
+/* Feather 32u4 w/wing
+#define RFM69_CS      10   // "B"
+#define RFM69_RST     11   // "A"
+#define RFM69_IRQ     2    // "SDA" (only SDA/SCL/RX/TX have IRQ!)
+#define RFM69_IRQN    digitalPinToInterrupt(RFM69_IRQ )
+*/
+
+/* Feather m0 w/wing 
+#define RFM69_CS      10   // "B"
+#define RFM69_RST     11   // "A"
+#define RFM69_IRQ     6    // "D"
+#define RFM69_IRQN    digitalPinToInterrupt(RFM69_IRQ )
+*/
+
+/* Teensy 3.x w/wing 
+#define RFM69_RST     9   // "A"
+#define RFM69_CS      10   // "B"
+#define RFM69_IRQ     4    // "C"
+#define RFM69_IRQN    digitalPinToInterrupt(RFM69_IRQ )
+*/
+
+/* WICED Feather w/wing 
+#define RFM69_RST     PA4     // "A"
+#define RFM69_CS      PB4     // "B"
+#define RFM69_IRQ     PA15    // "C"
+#define RFM69_IRQN    RFM69_IRQ
+*/
+
+#define LED           13  // onboard blinky
+//#define LED           0 //use 0 on ESP8266
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+RFM69 radio = RFM69(RFM69_CS, RFM69_IRQ, IS_RFM69HCW, RFM69_IRQN);
+
+void setup() {
+  while (!Serial); // wait until serial console is open, remove if not tethered to computer. Delete this line on ESP8266
+  Serial.begin(SERIAL_BAUD);
+    if (! matrix.begin()) {
+    Serial.println("IS31 not found");
+    while (1);
+  }
+  Serial.println("Feather RFM69HCW Receiver");
+  
+  // Hard Reset the RFM module
+  pinMode(RFM69_RST, OUTPUT);
+  digitalWrite(RFM69_RST, HIGH);
+  delay(100);
+  digitalWrite(RFM69_RST, LOW);
+  delay(100);
+  
+  // Initialize radio
+  radio.initialize(FREQUENCY,NODEID,NETWORKID);
+  if (IS_RFM69HCW) {
+    radio.setHighPower();    // Only for RFM69HCW & HW!
+  }
+  radio.setPowerLevel(31); // power output ranges from 0 (5dBm) to 31 (20dBm)
+  
+  //radio.encrypt(ENCRYPTKEY);
+  
+  pinMode(LED, OUTPUT);
+
+  Serial.print("\nListening at ");
+  Serial.print(FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
+  Serial.println(" MHz");
+}
+
+void loop() {
+  //check if something was received (could be an interrupt from the radio)
+  if (radio.receiveDone())
+  {
+    //print message received to serial
+    Serial.print('[');Serial.print(radio.SENDERID);Serial.print("] ");
+    Serial.print((char*)radio.DATA);
+    Serial.print("   [RX_RSSI:");Serial.print(radio.RSSI);Serial.print("]");
+
+    matrix.setTextSize(1);
+    matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
+    matrix.setRotation(0);
+    for (int8_t x=0; x>=-32; x--) {
+    matrix.clear();
+    uint16_t width();
+    matrix.setCursor(x,0);
+    matrix.print((char*)radio.DATA);
+    delay(5);
+
+    //check if received message contains Hello World
+    if ((char *)radio.DATA)
+    {
+      //check if sender wanted an ACK
+      if (radio.ACKRequested())
+      {
+        radio.sendACK();
+        Serial.println(" - ACK sent");
+      }
+      Blink(LED, 40, 3); //blink LED 3 times, 40ms between blinks
+
+  }
+    } 
+    delay(100); 
+  }
+  radio.receiveDone(); //put radio in RX mode
+  Serial.flush(); //make sure all serial data is clocked out before sleeping the MCU
+
+
+}
+
+void Blink(byte PIN, byte DELAY_MS, byte loops)
+{
+  for (byte i=0; i<loops; i++)
+  {
+    digitalWrite(PIN,HIGH);
+    delay(DELAY_MS);
+    digitalWrite(PIN,LOW);
+    delay(DELAY_MS);
+  }
+}
